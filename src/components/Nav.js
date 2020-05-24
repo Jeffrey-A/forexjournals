0d9a7261ee073927f3b1970db2f76d0fde08d180
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-05-24 17:47:42 -0400

    feat(Nav.js): keep track of the active page #14

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,37 +1,83 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 class Nav extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      activePage: "/",
+    };
+
+    this.changeActivePage = this.changeActivePage.bind(this);
   }
 
-  componentDidMount(){
-    console.log(window.location.pathname);
+  componentDidMount() {
+    const activePage = document.querySelector(
+      `a[href="${window.location.pathname}"]`
+    );
+
+    if (activePage) {
+      activePage.classList.add("active-page");
+    }
+  }
+
+  componentDidUpdate() {
+    document.querySelectorAll(".active-page").forEach((link) => {
+      link.classList.remove("active-page");
+    });
+
+    const activePage = document.querySelector(
+      `a[href="${window.location.pathname}"]`
+    );
+
+    if (activePage) {
+      activePage.classList.add("active-page");
+    }
+  }
+
+  changeActivePage(route) {
+    this.setState({ activePage: route });
   }
 
   render() {
-    
     return (
-      <div className='nav-main-container'>
+      <div className="nav-main-container">
         <ul className="nav-left-container">
           <li>
-            <Link to="/">Home</Link>
+            <Link onClick={() => this.changeActivePage("/")} to="/">
+              Home
+            </Link>
           </li>
         </ul>
         <ul className="nav-right-container">
           <li>
-            <Link to="/journals">Journals</Link>
+            <Link
+              onClick={() => this.changeActivePage("/journals")}
+              to="/journals"
+            >
+              Journals
+            </Link>
           </li>
           <li>
-            <Link to="/strategies">Strategies</Link>
+            <Link
+              onClick={() => this.changeActivePage("/strategies")}
+              to="/strategies"
+            >
+              Strategies
+            </Link>
           </li>
           <li>
-            <Link to="/login">Log in</Link>
+            <Link onClick={() => this.changeActivePage("/login")} to="/login">
+              Log in
+            </Link>
           </li>
           <li>
-            <Link to="/register">Register</Link>
+            <Link
+              onClick={() => this.changeActivePage("/register")}
+              to="/register"
+            >
+              Register
+            </Link>
           </li>
         </ul>
       </div>
