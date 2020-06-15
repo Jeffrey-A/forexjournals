aec88ccf89ff629a78deeb4673425331f4efb971
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-06-14 23:27:11 -0400

    feat(CreateStrategy): add placeholder text and appropriate classes #19

--- src/components/CreateStrategy.js
+++ src/components/CreateStrategy.js
@@ -26,26 +26,26 @@ class CreateStrategy extends React.Component {
       <div>
         <div className="modal-overlay"></div>
         <div className="modal-container">
-          <h1>Create Strategy</h1>
-          <button onClick={this.toggleModal}>X</button>
+          <h1 className='modal-header'>Create Strategy</h1>
+          <span className='close-modal-btn' onClick={this.toggleModal}>X</span>
           <div className="create-strategy-inputs-container">
-            <input />
-            <textarea></textarea>
+            <input className='modal-input' placeholder='Name'/>
+            <textarea className='modal-textarea' placeholder='Description'></textarea>
             <select>
-              <option>Option 1</option>
+              <option>Indicators</option>
               <option>Option 2</option>
               <option>Option 3</option>
             </select>
             <select>
-              <option>Option 1</option>
+              <option>Time frames</option>
               <option>Option 2</option>
               <option>Option 3</option>
             </select>
-            <textarea></textarea>
-            <textarea></textarea>
-            <input />
-            <input />
-            <button>Create</button>
+            <textarea className='modal-textarea' placeholder='Entry conditions'></textarea>
+            <textarea className='modal-textarea' placeholder='Exit conditions'></textarea>
+            <input className='modal-input' placeholder='Risk per trade' />
+            <input className='modal-input' placeholder='Risk to reward' />
+            <button className='modal-create-btn'>Create</button>
           </div>
         </div>
       </div>
