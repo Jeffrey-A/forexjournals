baef5fc33d6e9b8f7c352498afc8906d90d37fee
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-07-05 22:04:36 -0400

    feat(StrategyCard): create card skeleton

---
+++ src/components/StrategyCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class StrategyCard extends React.Component {
+  render() {
+    const { name } = this.props;
+
+    return (
+      <div classNAme="strategy-card-main-container">
+        <div className="strategy-card-name-container">
+          <span>{name}</span>
+        </div>
+        <div className="strategy-card-icons-container">
+          <span>View</span>
+          <span>Edit</span>
+          <span>Delete</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default StrategyCard;
