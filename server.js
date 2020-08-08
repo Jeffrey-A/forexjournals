e4ed58ad0208efcbd8dd616a09629c4be3c8a7ca
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-08-07 21:47:41 -0400

    feat: move journal routes to journalsRoutes.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var passport = require("passport");
 const bcrypt = require("bcrypt");
 const logErrorMessage = require("./server-utils/utils");
 const strategyRouter = require("./routes/strategiesRoutes");
+const journalRouter = require("./routes/journalsRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -44,6 +45,7 @@ app.use(passport.session());
 db.sync().then(() => console.log("connected to database successfully"));
 
 app.use("/strategies", strategyRouter);
+app.use("/journals", journalRouter);
 // Routes
 app.post("/login", passport.authenticate("local"), (req, res) => {
   const { id, user_name, email } = req.user;
@@ -82,91 +84,6 @@ app.post("/register", async (req, res) => {
     })
   );
 });
-
-app.get("/journals/:userId/:strategyId?", ensureAuthenticated, (req, res) => {
-  const user_id = req.params.userId;
-  const strategy_id = req.params.strategyId;
-
-  if (strategy_id) {
-    Journals.findAll({ where: { user_id, strategy_id } })
-      .then((journals) => {
-        res.json(journals);
-      })
-      .catch((err) => {
-        logErrorMessage("Error getting journals", err);
-        res.sendStatus(500);
-      });
-  } else {
-    Journals.findAll({ where: { user_id } })
-      .then((journals) => {
-        res.json(journals);
-      })
-      .catch((err) => {
-        logErrorMessage("Error getting journals", err);
-        res.sendStatus(500);
-      });
-  }
-});
-
-app
-  .route("/journals/:userId/:strategyId")
-  .post(ensureAuthenticated, (req, res) => {
-    const payload = {
-      user_id: req.params.userId,
-      strategy_id: req.params.strategyId,
-      errors: req.body.errors,
-      pair: req.body.pair,
-      comments: req.body.comments,
-      order_type: req.body.order_type,
-      pips_gained_lost: req.body.pips_gained_lost,
-      img_link: req.body.img_link,
-    };
-
-    Journals.create(payload)
-      .then(() => {
-        res.sendStatus(200);
-      })
-      .catch((err) => {
-        logErrorMessage("Error creating journal, err");
-        res.send(err);
-      });
-  })
-  .put(ensureAuthenticated, (req, res) => {
-    const strategy_id = req.params.strategyId;
-    const user_id = req.params.userId;
-    const journal_id = req.body.journal_id ? req.body.journal_id : "";
-    const updatedPayload = {
-      pair: req.body.pair,
-      comments: req.body.comments,
-      order_type: req.body.order_type,
-      errors: req.body.errors,
-      pips_gained_lost: req.body.pips_gained_lost,
-      img_link: req.body.img_link,
-    };
-
-    Journals.update(updatedPayload, {
-      where: { journal_id, strategy_id, user_id },
-    })
-      .then(() => {
-        res.sendStatus(200);
-      })
-      .catch((err) => {
-        logErrorMessage("Error updating journal", err);
-        res.sendStatus(500);
-      });
-  })
-  .delete(ensureAuthenticated, (req, res) => {
-    const strategy_id = req.params.strategyId;
-    const user_id = req.params.userId;
-    const journal_id = req.body.journal_id ? req.body.journal_id : "";
-
-    Journals.destroy({ where: { journal_id, user_id, strategy_id } })
-      .then(() => res.sendStatus(200))
-      .catch((err) => {
-        logErrorMessage("Error deleting journal", err);
-        res.sendStatus(500);
-      });
-  });
 
 // Handle react-router routes
 app.get("*", (req, res) => {
