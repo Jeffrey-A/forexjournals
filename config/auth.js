858c3c3d80bebd79813241b0276efcc28769adc1
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-08-30 09:17:52 -0400

    feat(config/auth.j): complete ensureAuthenticated middleware

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,33 @@
-// TODO: this function should use JWT to ensure the user is authenticated.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+
+const jwtSecret = process.env.JWT_SECRET;
+
+async function ensureAuthenticated(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.sendStatus(401);
   }
-  res.sendStatus(401);
+
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  try {
+    await promisify(jwt.verify)(token, jwtSecret);
+    next();
+  } catch (e) {
+    if (e.name === 'JsonWebTokenError') {
+      res.status(401).json({ status: 'fail', message: 'Invalid token' });
+    }
+
+    if (e.name === 'TokenExpiredError') {
+      res.status(401).json({ status: 'fail', message: 'Token has expired' });
+    }
+  }
 }
 
 module.exports = ensureAuthenticated;
